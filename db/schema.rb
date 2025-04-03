cc1a7ec797d0eab55c9450a593d5bdd802d6699f
Joé Dupuis <joe@dupuis.io>
2025-04-03 16:56:59 -0700

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_28_022800) do
+  create_table "requests", force: :cascade do |t|
+    t.string "uid"
+    t.string "ip"
+    t.string "method"
+    t.string "url"
+    t.json "parameters"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_requests_on_uid", unique: true
+  end
 end
