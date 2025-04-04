8393098496bd65846cb1cf2a7f57de4b28b80829
Joé Dupuis <joe@dupuis.io>
2025-04-03 17:43:29 -0700

    Allow turning off yjit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SqliteScale
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.yjit = ENV["RUBY_YJIT_ENABLE"] == "1"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
