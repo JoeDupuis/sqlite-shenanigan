390ae7b9eb521296b71198dcf0ae1e690efad1e0
Joé Dupuis <joe@dupuis.io>
2025-04-05 19:39:59 -0700

    Something

---
+++ aaa.rb
@@ -0,0 +1,13 @@
+require 'sqlite3'
+require 'benchmark'
+
+db = SQLite3::Database.new("test.db")
+db.execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, data TEXT)")
+
+puts Benchmark.measure {
+  db.transaction
+  1_000_000.times do |i|
+    db.execute("INSERT INTO test (data) VALUES (?)", ["Row #{i}"])
+  end
+  db.commit
+}
